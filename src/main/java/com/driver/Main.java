ce3cd9aa6496daac4cb81335b6784e7aeb604fb2
Aishwarya Heshannavar <aishwaryaheshannavar@gmail.com>
2024-02-14 14:57:03 +0530

    "Changes are done in the code"

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,30 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y,int z) {
+            return x*y*z;
+        }
+        public double product(double x,double y){
+            return x*y;
+        }
+    }
+    public static void main(String args[]){
+        Product p=new Product();
+        int a= p.product(2,3);
+        System.out.println(a);
+        int b= p.product(2,3,4);
+        System.out.println(b);
+        double c= p.product(2.0,3.0);
+        System.out.println(c);
+
+
+    }
     //done is working
+
+
 
 }
